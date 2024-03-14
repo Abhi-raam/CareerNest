d0570d674c46ec3921432dc048414103b004cb0f
Abhiram Suresh <abhiramunni246@gmail.com>
2024-03-14 22:41:21 +0530

    user profile edit added

---
+++ src/Components/User/UserEdit.tsx
@@ -0,0 +1,145 @@
+import { useState } from "react"
+
+function UserEdit() {
+    const [firstName ,setFirstName] = useState<string>("")
+    const [lastName ,setLastName] = useState<string>("")
+    const [mobile ,setMobile] = useState<string>("")
+    const [email ,setEmail] = useState<string>("")
+    const [dept ,setDept] = useState<string>("")
+    const [job ,setJob] = useState<string>("")
+    const [gender ,setGender] = useState<string>("")
+    const [dob ,setDob] = useState<string>("")
+    const [district ,setDistrict] = useState<string>("")
+    const [state,setState] = useState<string>("")
+    const [about,setAbout] = useState<string>("")
+
+    const submitBtn =(event: React.FormEvent)=>{
+        event.preventDefault();
+        console.log(firstName);
+        console.log(lastName);
+        console.log(mobile);
+        console.log(email);
+        console.log(dept);
+        console.log(job);
+        console.log(gender);
+        console.log(dob);
+        console.log(district);
+        console.log(state);
+        console.log(about);
+    }
+
+    return (
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6  space-y-4 lg:space-y-5">
+            <form action="" className="space-y-4 lg:space-y-5" onSubmit={submitBtn}>
+                <div className='lg:flex justify-around pt-5 space-y-4 lg:space-y-0'>
+                    <div className='flex justify-between  lg:pl-5  w-full md:px-10'>
+                        <label className='text-base font-semibold text-gray-600'>First Name</label>
+                        <input type="text" placeholder="First Name" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg" onChange={(event)=>{setFirstName(event.target.value)}} />
+                    </div>
+                    <div className='flex justify-between  lg:pl-5  w-full md:px-10'>
+                        <h4 className='text-base  font-semibold text-gray-600'>Last Name</h4>
+                        <input type="text" placeholder="Last Name" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setLastName(event.target.value)}} />
+                    </div>
+                </div>
+                <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                    <div className='flex justify-between  w-full md:px-10'>
+                        <h4 className='text-base  font-semibold text-gray-600'>Mobile</h4>
+                        <input type="text" placeholder="Mobile No." className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setMobile(event.target.value)}} />
+                    </div>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>Email</h4>
+                        <input type="email" placeholder="Email" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setEmail(event.target.value)}} />
+                    </div>
+                </div>
+                <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>Department</h4>
+                        <input type="text" placeholder="Department" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setDept(event.target.value)}} />
+                    </div>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>Job Title</h4>
+                        <input type="text" placeholder="Job Title" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setJob(event.target.value)}} />
+                    </div>
+                </div>
+                <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>Gender</h4>
+                        <input type="text" placeholder="Gender" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setGender(event.target.value)}} />
+                    </div>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>DOB</h4>
+                        <input type="date" placeholder="Date of Birth" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setDob(event.target.value)}} />
+                    </div>
+                </div>
+                <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>District</h4>
+                        <input type="text" placeholder="District" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setDistrict(event.target.value)}} />
+                    </div>
+                    <div className='flex justify-between  lg:pl-10  w-full md:px-10'>
+                        <h4 className='text-base font-semibold text-gray-600'>State</h4>
+                        <input type="text" placeholder="State" className=" w-1/2 input-bordered outline-none input-sm border-b-2 border-0 max-w-lg"onChange={(event)=>{setState(event.target.value)}} />
+                    </div>
+                </div>
+
+                <div className='flex  justify-between lg:space-x-[9rem] lg:pl-10 w-full md:px-10'>
+                    <h4 className='text-base font-semibold text-gray-600'>About</h4>
+                    <textarea placeholder="About" className="textarea textarea-bordered textarea-md w-1/2 lg:w-full" onChange={(event)=>{setAbout(event.target.value)}}></textarea>
+                </div>
+
+                <div className="grid grid-cols-1 lg:grid-cols-1 gap-2">
+                    <div className='flex justify-between lg:pl-10 w-full md:px-10'>
+                        <label className="form-control w-full max-w-xs text-base font-semibold text-gray-600">Profile Picture
+                            <input type="file" className="file-input file-input-bordered file-input-sm  max-w-lg" />
+                            <div className="label">
+                                <span className="label-text-alt">Only JPEG,JPG,PNG allowed</span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+                    <div className=' lg:pl-9 w-full md:px-10'>
+                        <label className="form-control w-full max-w-xs text-base font-semibold text-gray-600">Resume
+                            <input type="file" className="file-input file-input-bordered file-input-sm  max-w-lg" />
+                            <div className="label">
+                                <span className="label-text-alt">Only PDF allowed</span>
+                            </div>
+                        </label>
+                    </div>
+                    <div className=' lg:pl-5 w-full md:px-10'>
+                        <label className="form-control w-full max-w-xs text-base font-semibold text-gray-600">Degree
+                            <input type="file" className="file-input file-input-bordered file-input-sm  max-w-lg" />
+                            <div className="label">
+                                <span className="label-text-alt">Only PDF allowed</span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+                    <div className=' lg:pl-9 w-full md:px-10'>
+                        <label className="form-control w-full max-w-xs text-base font-semibold text-gray-600">12th
+                            <input type="file" className="file-input file-input-bordered file-input-sm  max-w-lg" />
+                            <div className="label">
+                                <span className="label-text-alt">Only PDF allowed</span>
+                            </div>
+                        </label>
+                    </div>
+                    <div className='lg:pl-5 w-full md:px-10'>
+                        <label className="form-control w-full max-w-xs text-base font-semibold text-gray-600">10th
+                            <input type="file" className="file-input file-input-bordered file-input-sm  max-w-lg" />
+                            <div className="label">
+                                <span className="label-text-alt">Only PDF allowed</span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <button className="btn bg-green-500 text-white hover:bg-green-600" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default UserEdit
--- src/Components/User/UserProfileCard.tsx
+++ src/Components/User/UserProfileCard.tsx
@@ -1,7 +1,8 @@
 import profilePic from '../../assets/profilepic.jpeg'
+import UserEdit from './UserEdit'
 function UserProfileCard() {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 lg:h-screen">
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:h-auto min-h-screen">
             <div className="pt-10">
                 <div className='flex justify-center'>
                     <img src={profilePic} className='rounded-full w-[9rem]' alt="" />
@@ -22,70 +23,95 @@ function UserProfileCard() {
                         </p>
                     </div>
                 </div>
-                <div className='pt-10 pl-6'>
-                    <h3 className='text-blue_secondary text-xl font-semibold text-start'>Skills</h3>
-                    <div>
-                        <ul className="list-disc text-sm pt-3 text-gray-700 font-medium list-outside px-4">
-                            <li>MongoDB</li>
-                            <li>ExpressJs</li>
-                            <li>ReactJs</li>
-                            <li>NodeJs</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                        </ul>
+            </div>
+            
+            <div className="px-3 pt-6 col-span-2 pb-4">
+                <div role="tablist" className="tabs tabs-lifted">
+
+                    <input type="radio" name="my_tabs_2" role="tab" className="tab text-base lg:text-lg font-semibold" aria-label="Account Settings" defaultChecked />
+                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6 h-[30rem] space-y-4 lg:space-y-5">
+                        <div className='lg:flex justify-around pt-5 space-y-4 lg:space-y-0'>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>First Name</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Steve Rogers</h4>
+                            </div>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Last Name</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Steve Rogers</h4>
+                            </div>
+                        </div>
+                        <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Mobile</h4>
+                                <h4 className='border-b-2 border-gray-400  lg:w-1/2 font-medium '>+91 9999999999</h4>
+                            </div>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Email</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>sample@gmail.com</h4>
+                            </div>
+                        </div>
+                        <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Department</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2 font-medium'>CSE</h4>
+                            </div>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Job Title</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Web Developer</h4>
+                            </div>
+                        </div>
+                        <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>Gender</h4>
+                                <h4 className='border-b-2 border-gray-400  w-1/2 font-medium '>Male</h4>
+                            </div>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>DOB</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>01 Jan 2000</h4>
+                            </div>
+                        </div>
+                        <div className='lg:flex justify-around space-y-4 lg:space-y-0'>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>District</h4>
+                                <h4 className='border-b-2 border-gray-400  w-1/2 font-medium '>Kottayam</h4>
+                            </div>
+                            <div className='flex justify-around px-10  w-full'>
+                                <h4 className='text-base font-semibold text-gray-600'>State</h4>
+                                <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Kerala</h4>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="px-3 space-y-5 pt-6">
-                <div>
-                    <h3 className='text-blue_secondary text-xl font-semibold text-start'>Account Settings</h3>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Name</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Steve Rogers</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Job Title</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Web Developer</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Mobile</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>+91 9999999999</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Email</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>sample@gmail.com</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Genter</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Male</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Date of Birth</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>01 Jan 2000</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>District</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Kottayam</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>State</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>Kerala</h4>
-                </div>
-                <div className='flex justify-between px-10'>
-                    <h4 className='text-base font-semibold text-gray-600'>Country</h4>
-                    <h4 className='border-b-2 border-gray-400 w-1/2  font-medium '>India</h4>
-                </div>
-            </div>
-            <div className="px-3 space-y-5 pt-6">
-                <div>
-                    <h3 className='text-blue_secondary text-xl font-semibold text-start'>Certificates</h3>
-                </div>
-                <div className='grid grid-cols-2 lg:grid-cols-3 gap-4 pb-5'>
-                    <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>Resume</button>
-                    <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>10th</button>
-                    <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>12th</button>
-                    <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>Degree</button>
+
+                    <input type="radio" name="my_tabs_2" role="tab" className="tab text-base lg:text-lg font-semibold" aria-label="Certifications and Skills" />
+                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6 h-[30rem]">
+                        <div className='pt-5 pl-6'>
+                            <h3 className='text-blue_secondary text-xl font-semibold text-start'>Skills</h3>
+                            <div>
+                                <ul className="list-disc text-sm pt-3 text-gray-700 font-medium list-outside px-4">
+                                    <li>MongoDB</li>
+                                    <li>ExpressJs</li>
+                                    <li>ReactJs</li>
+                                    <li>NodeJs</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="px-3 space-y-5 pt-6">
+                            <div>
+                                <h3 className='text-blue_secondary text-xl font-semibold text-start'>Certificates</h3>
+                            </div>
+                            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 pb-5'>
+                                <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>Resume</button>
+                                <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>Degree</button>
+                                <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>12th</button>
+                                <button className='btn bg-blue_secondary text-blue_main hover:bg-blue-900 shadow-md'>10th</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <input type="radio" name="my_tabs_2" role="tab" className="tab text-base lg:text-lg font-semibold" aria-label="Edit" />
+                    <UserEdit />
                 </div>
             </div>
         </div>
