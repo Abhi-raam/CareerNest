ad7f42003c0518f58beb9956b2024030eae6fef3
Abhiram Suresh <abhiramunni246@gmail.com>
2024-03-17 22:42:36 +0530

    api integrated error fixed

--- src/Pages/User/UserInternshipTab.tsx
+++ src/Pages/User/UserInternshipTab.tsx
@@ -1,17 +1,8 @@
-import JobCard from "../../Components/JobCard"
 
 function UserInternshipTab() {
     return (
         <div className='grid md:grid-cols-2 lg:grid-cols-2 gap-5 lg:h-[76vh] overflow-y-scroll scrollbar-hidden justify-items-center'>
-            <JobCard name={"C#"} />
-            <JobCard name={"Python"} />
-            <JobCard name={"Java"} />
-            <JobCard name={"C++"} />
-            <JobCard name={"javaScript"} />
-            <JobCard name={"javaScript"} />
-            <JobCard name={"javaScript"} />
-            <JobCard name={"javaScript"} />
-            <JobCard name={"javaScript"} />
+           page under construction
         </div>
     )
 }
